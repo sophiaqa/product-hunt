21179bf1c7aa7c2b9f60332166153823f2495638
Sofiia Herman <sophiaqa888@gmail.com>
2018-07-27 09:21:44 +0300

    update for product model

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
+
     title = models.CharField(max_length=100)
     url = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
